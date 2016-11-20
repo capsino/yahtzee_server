ef6ecc4697761f455bf73e96c62e6d669b02e778
Jon Jagger <jon@jaggersoft.com>
2016-11-20 10:43:42 +0000

    ensure default is that [docker build] leaves behind no exited containers

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 docker build --tag jj/sparkexample .
+status=$?
+
+# if the build fails Docker removes all intermediate containers except the one for
+# the Dockerfile command that actually failed. Remove that too
+if [ ${status} != 0 ]; then
+  docker ps --all --quiet --filter='status=exited' | xargs docker rm --force > /dev/null
+fi
+
+exit ${status}
