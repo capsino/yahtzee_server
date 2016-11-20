68aed13fc5a228d673327d277abe527f519853bf
Jon Jagger <jon@jaggersoft.com>
2016-11-20 15:50:18 +0000

    make a start on returning json from get request

--- src/main/java/sparkexample/Service.java
+++ src/main/java/sparkexample/Service.java
@@ -5,8 +5,12 @@ import static spark.Spark.get;
 public class Service {
 
     public static void main(String[] args) {
-        get("/", (req, res) -> {
-            return Greeting.message();
+        get("/", "application/json", (req, res) -> {
+            return quoted(Greeting.message());
         });
+    }
+
+    private static String quoted(String s) {
+      return "\"" + s + "\"";
     }
 }
