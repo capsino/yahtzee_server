0814076dee40391592d2bcf5606a92aecca611b8
Jon Jagger <jon@jaggersoft.com>
2016-11-20 12:14:49 +0000

    try again

--- run.sh
+++ run.sh
@@ -6,14 +6,13 @@ cid=$(docker run \
   --publish 4567:4567 \
   jj/sparkexample)
 
-ip=$(docker inspect ${cid} | grep IPAddress | cut -d '"' -f 4)
+ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${cid})
 port=4567
 address=http://${ip}:${port}
 echo ${address}
 
 # crude way to wait till container is up
-sleep 2
-
+sleep 3
 curl ${address}
 echo
 docker rm --force ${cid} > /dev/null
