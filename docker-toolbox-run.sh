533d8a8e6004dd99f703bbc785e932de3a008e70
Jon Jagger <jon@jaggersoft.com>
2016-11-21 19:59:29 +0000

    run Dockerfile maven commands more quietly

--- docker-toolbox-run.sh
+++ docker-toolbox-run.sh
@@ -2,6 +2,7 @@
 
 client_ip=$(docker-machine ip default)
 server_ip=$(docker-machine ip default)
+
 curl --include --header "Accept: application/json" http://${client_ip}:80
 echo
 curl --include --header "Accept: application/json" http://${server_ip}:4567
