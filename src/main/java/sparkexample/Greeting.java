294087ab2de61b3dc037324d9f0f1fde079d1291
Jon Jagger <jon@jaggersoft.com>
2016-11-21 17:55:14 +0000

    use Gson to create json returns by micro-service; add test showing example of use

--- src/main/java/sparkexample/Greeting.java
+++ src/main/java/sparkexample/Greeting.java
@@ -1,14 +1,12 @@
 package sparkexample;
 
 import org.apache.http.client.methods.HttpGet;
-import com.google.gson.JsonParser;
 
 public class Greeting {
 
     public static String message() {
 
         HttpGet httpget = new HttpGet("http://httpbin.org/");
-        JsonParser jsonParser = new JsonParser();
 
         return "Hello from http://sparkjava.com";
     }
--- src/main/java/sparkexample/Service.java
+++ src/main/java/sparkexample/Service.java
@@ -1,16 +1,17 @@
 package sparkexample;
 
 import static spark.Spark.get;
+import com.google.gson.Gson;
+import java.util.HashMap;
 
 public class Service {
 
     public static void main(String[] args) {
         get("/", "application/json", (req, res) -> {
-            return "{" + quoted("message") + ":" + quoted(Greeting.message()) + "}";
+            HashMap<String,Object> result = new HashMap<String,Object>();
+            result.put( "message", Greeting.message() );
+            return new Gson().toJson(result);
         });
     }
 
-    private static String quoted(String s) {
-      return "\"" + s + "\"";
-    }
 }
