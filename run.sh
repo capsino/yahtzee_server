319e94084b648b0814d73e62e667387e817a8224
James Relph <james.relph@capgemini.com>
2016-11-23 15:36:43 +0000

    IP testing

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' yahtzee_server)
+echo $ip
 curl --include --header "Accept: application/json" http://${ip}:4567
 echo
