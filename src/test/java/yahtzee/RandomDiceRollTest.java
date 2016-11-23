5178c84d6adf27c46ca36d76f8da923f7c2117b0
John <john.eccleston@capgemini.com>
2016-11-23 11:57:26 +0000

    more tests

--- src/test/java/yahtzee/RandomDiceRollTest.java
+++ src/test/java/yahtzee/RandomDiceRollTest.java
@@ -8,9 +8,11 @@ public class RandomDiceRollTest {
 
 	@Test
 	public void roll_returns_number_between_1_and_6() {
-		
-		int output = RandomDiceRoll.roll();
-		
+		int output;
+		for(int i=0; i<1000; i++){
+			output = RandomDiceRoll.roll();
+			assertTrue(output <= 6 && output >= 1);
+		}
 	}
 
 }
